dd0ccc5f1bf792a0ca739ac003f25df3a6769946
kyutekrap <katepark1225@naver.com>
2025-02-23 03:04:12 +0900

    v0.0.6

---
+++ util/ui.d.ts
@@ -0,0 +1,9 @@
+export class UI {
+    static closeDrawer(): void;
+    static closeModal(): void;
+    static closeSnackbar(): void;
+    static openDrawer(): void;
+    static openModal(...args: any[]): void;
+    static openSnackbar(text: any): void;
+    static toggleDarkMode(): void;
+}
