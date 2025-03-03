e286199d9cb29de5985c1f49890d120a47078f61
kyutekrap <katepark1225@naver.com>
2025-03-03 22:36:54 +0900

    v0.0.9

--- src/HomeView/ext/DrawerButton.BgButtonExt.ts
+++ src/HomeView/ext/DrawerButton.BgButtonExt.ts
@@ -1,10 +1,11 @@
-import { BgButtonBase } from "vanilla-js-ui-kit/base";
-import { BgButtonProps } from "vanilla-js-ui-kit/props";
-import { UI } from "vanilla-js-ui-kit/util";
+import { BgButtonBase } from "../../../base";
+import { BgButtonProps } from "../../../props";
+import { UI } from "../../../util";
 
 export class DrawerButton extends BgButtonBase {
     constructor(props: BgButtonProps) {
         super(props);
+        this._bgButton.addEventListener("click", () => this.onClick());
     }
 
     onClick() {
